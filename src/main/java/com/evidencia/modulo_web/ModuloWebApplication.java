504b2628f3bace42eb4296643143cd334afae3b2
Alexandretta <alxsalazar21@gmail.com>
2025-03-14 17:50:53 -0500

    se inicializa repositorio con proyecto crud en Java

---
+++ src/main/java/com/evidencia/modulo_web/ModuloWebApplication.java
@@ -0,0 +1,13 @@
+package com.evidencia.modulo_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloWebApplication.class, args);
+	}
+
+}
