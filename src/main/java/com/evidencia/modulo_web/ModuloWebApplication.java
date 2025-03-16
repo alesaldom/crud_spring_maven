e54d86c8d457859f34dd5b272aea542b9bede31b
Alexandretta <alxsalazar21@gmail.com>
2025-03-16 10:11:26 -0500

    se agregan properties

--- src/main/java/com/evidencia/modulo_web/ModuloWebApplication.java
+++ src/main/java/com/evidencia/modulo_web/ModuloWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ModuloWebApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ModuloWebApplication.class, args);
 	}
